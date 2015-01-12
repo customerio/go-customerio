b2937ee9cd0577224a53c240e07086dc618548f6
Stephen Young <me@hownowstephen.com>
2015-01-12 17:11:10 +0000

    base library, handles identify, track and delete

---
+++ customerio.go
@@ -0,0 +1,125 @@
+package customerio
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"path"
+	"strconv"
+)
+
+// CustomerIO wraps the customer.io API, see: http://customer.io/docs/api/rest.html
+type CustomerIO struct {
+	siteID string
+	apiKey string
+}
+
+// CustomerIOError is returned by any method that fails at the API level
+type CustomerIOError struct {
+	status int
+	url    string
+	body   []byte
+}
+
+func (e *CustomerIOError) Error() string {
+	return fmt.Sprintf("%v: %v %v", e.status, e.url, string(e.body))
+}
+
+// NewCustomerIO creates a new CustomerIO object to perform requests on the supplied credentials
+func NewCustomerIO(siteID, apiKey string) *CustomerIO {
+	return &CustomerIO{siteID, apiKey}
+}
+
+// Identify identifies a customer and sets their attributes
+func (c *CustomerIO) Identify(customerID string, attributes map[string]interface{}) error {
+	j, err := json.Marshal(attributes)
+
+	if err != nil {
+		return err
+	}
+
+	status, responseBody, err := c.request("PUT", c.customerURL(customerID), j)
+
+	if err != nil {
+		return err
+	} else if status != 200 {
+		return &CustomerIOError{status, c.customerURL(customerID), responseBody}
+	}
+
+	return nil
+}
+
+// Track sends a single event to Customer.io for the supplied user
+func (c *CustomerIO) Track(customerID string, eventName string, data map[string]interface{}) error {
+
+	body := map[string]interface{}{"name": eventName, "data": data}
+	j, err := json.Marshal(body)
+
+	if err != nil {
+		return err
+	}
+
+	status, responseBody, err := c.request("POST", c.eventURL(customerID), j)
+
+	if err != nil {
+		return err
+	} else if status != 200 {
+		return &CustomerIOError{status, c.eventURL(customerID), responseBody}
+	}
+
+	return nil
+}
+
+// Delete deletes a customer
+func (c *CustomerIO) Delete(customerID string) error {
+	status, responseBody, err := c.request("DELETE", c.customerURL(customerID), []byte{})
+
+	if err != nil {
+		return err
+	} else if status != 200 {
+		return &CustomerIOError{status, c.customerURL(customerID), responseBody}
+	}
+
+	return nil
+}
+
+func (c *CustomerIO) auth() string {
+	return base64.URLEncoding.EncodeToString([]byte(fmt.Sprintf("%v:%v", c.siteID, c.apiKey)))
+}
+
+func (c *CustomerIO) host() string {
+	return "track.customer.io"
+}
+
+func (c *CustomerIO) customerURL(customerID string) string {
+	return "https://" + path.Join(c.host(), "api/v1", "customers", customerID)
+}
+
+func (c *CustomerIO) eventURL(customerID string) string {
+	return "https://" + path.Join(c.host(), "api/v1", "customers", customerID, "events")
+}
+
+func (c *CustomerIO) request(method, url string, body []byte) (status int, responseBody []byte, err error) {
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
+	if err != nil {
+		return
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Basic %v", c.auth()))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Content-Length", strconv.Itoa(len(body)))
+
+	resp, err := http.DefaultClient.Do(req)
+	status = resp.StatusCode
+	if err == nil {
+		defer resp.Body.Close()
+
+		responseBody = make([]byte, resp.ContentLength)
+		resp.Body.Read(responseBody)
+	}
+
+	return
+
+}
---
+++ customerio_test.go
@@ -0,0 +1,97 @@
+package customerio
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+)
+
+var cio *CustomerIO
+
+func TestMain(m *testing.M) {
+
+	siteID := os.Getenv("CUSTOMERIO_SITE_ID")
+	apiKey := os.Getenv("CUSTOMERIO_API_KEY")
+
+	var exitCode int
+
+	if siteID != "" && apiKey != "" {
+		cio = NewCustomerIO(siteID, apiKey)
+		exitCode = m.Run()
+	} else {
+		exitCode = 1
+		fmt.Println("Must set CUSTOMERIO_SITE_ID and CUSTOMERIO_API_KEY environment variables to test this library")
+	}
+
+	os.Exit(exitCode)
+}
+
+func TestIdentify(t *testing.T) {
+
+	attributes := map[string]interface{}{}
+
+	err := cio.Identify("golang-test-noattributes", attributes)
+	defer cio.Delete("golang-test-noattributes")
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	attributes["email"] = "golang@customer.io"
+	attributes["first_name"] = "golang"
+	attributes["last_name"] = "testsuite"
+
+	err = cio.Identify("golang-test-stringattributes", attributes)
+	defer cio.Delete("golang-test-stringattributes")
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	attributes["paid"] = true
+	attributes["numUsers"] = 10
+
+	err = cio.Identify("golang-test-mixed-attributes", attributes)
+	defer cio.Delete("golang-test-mixed-attributes")
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	attributes["_last_visit"] = time.Now().Unix()
+
+	err = cio.Identify("golang-test-magic-attributes", attributes)
+	defer cio.Delete("golang-test-magic-attributes")
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestTrack(t *testing.T) {
+	cio.Identify("golang-test-events", map[string]interface{}{})
+	defer cio.Delete("golang-test-events")
+
+	err := cio.Track("golang-test-events", "golang-test", map[string]interface{}{})
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	err = cio.Track("golang-test-events", "golang-test-data", map[string]interface{}{"value": 1, "name": "event"})
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+}
+
+func TestDelete(t *testing.T) {
+	cio.Identify("golang-test-delete", map[string]interface{}{})
+
+	err := cio.Delete("golang-test-delete")
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
