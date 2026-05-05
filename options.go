6cdceb8348ee6e7c123d5f9978cc9cdad5e0a743
Stephen Young <stephen@customer.io>
2026-05-04 23:10:41 -0400

    add funcopts for setting other fields

--- options.go
+++ options.go
@@ -1,5 +1,7 @@
 package customerio
 
+import "time"
+
 type option struct {
 	api   func(*APIClient)
 	track func(*CustomerIO)
@@ -52,4 +54,50 @@ func WithUserAgent(ua string) option {
 			c.UserAgent = ua
 		},
 	}
+}
+
+// TrackOption sets optional top-level fields on tracked events.
+type TrackOption func(map[string]interface{})
+
+// TrackType is the type of event being tracked.
+type TrackType string
+
+const (
+	TrackTypeEvent  TrackType = "event"
+	TrackTypePage   TrackType = "page"
+	TrackTypeScreen TrackType = "screen"
+)
+
+// WithEventID sets the id field on tracked events.
+func WithEventID(id string) TrackOption {
+	return func(payload map[string]interface{}) {
+		payload["id"] = id
+	}
+}
+
+// WithEventTimestamp sets the timestamp field on tracked events.
+func WithEventTimestamp(timestamp time.Time) TrackOption {
+	return func(payload map[string]interface{}) {
+		payload["timestamp"] = timestamp.Unix()
+	}
+}
+
+// WithEventType sets the type field on tracked events.
+func WithEventType(typ TrackType) TrackOption {
+	return func(payload map[string]interface{}) {
+		payload["type"] = typ
+	}
+}
+
+func trackPayload(eventName string, data map[string]interface{}, opts ...TrackOption) map[string]interface{} {
+	payload := map[string]interface{}{
+		"name": eventName,
+		"data": data,
+	}
+
+	for _, opt := range opts {
+		opt(payload)
+	}
+
+	return payload
 }
